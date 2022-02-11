42c5a27e5af8e3dc362eaa68c7349bcbf99c1403
Asif Ridwan <asifridwan@gmail.com>
2022-02-11 20:26:09 +0600

    Added logo and favicon to frontend

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,23 @@
+import logo from "../images/logo.png";
+
 export default function Navbar() {
   return (
     <nav>
-        <span className="logo">Browse Restaurants</span>
-        <div>
-            <input className="search-box" type="text" placeholder="Enter Name, Date or Time..."/>
-            <button className="search-button">Search</button>
-        </div>
-        <div>
-            <label htmlFor="" className="filter-select">Filter By : </label>
-            <select name="" id="" className="filter-type">
-                <option value="">Name</option>
-                <option value="">Date and Time</option>
-            </select>
-        </div>
+      <div className="logo">
+        <img className="logo-img" src={logo} alt="" />
+        <span className="logo-text">Browse Restaurants</span>
+      </div>
+      <div>
+        <input className="search-box" type="text" placeholder="Enter Name, Date or Time..."/>
+        <button className="search-button">Search</button>
+      </div>
+      <div>
+        <label htmlFor="" className="filter-select">Filter By : </label>
+        <select name="" id="" className="filter-type">
+          <option value="">Name</option>
+          <option value="">Date and Time</option>
+        </select>
+      </div>
     </nav>
   )
 }
