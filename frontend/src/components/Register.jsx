ec89f547b022c4b7f224e4fc0f18d5ff8f8d4c0d
Asif Ridwan <asifridwan@gmail.com>
2022-02-24 21:07:39 +0600

    Separated Loading Spinner into a separate component

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,19 +1,19 @@
 export default function Register() {
   return (
     <div className="form-container">
-        <div className="intro-logo">
-            <div className="logo-icon-dark"></div>
-            <div className="logo-text-dark">Browse Restaurants</div>
-        </div>
-        <div className="registration-form">
-            <p className="form-name">Registration</p>
-            <input className="form-input" type="text" placeholder="Username" />
-            <input className="form-input" type="text" placeholder="Email Address" />
-            <input className="form-input" type="password" placeholder="Password" />
-            <input className="form-input" type="password" placeholder="Confirm Password" />
-            <button className="submit-button">Register</button>
-            <p className="form-switcher">Already have an account ? <span>Login</span></p>
-        </div>
+      <div className="intro-logo">
+        <div className="logo-icon-dark"></div>
+        <div className="logo-text-dark">Browse Restaurants</div>
+      </div>
+      <div className="registration-form">
+        <p className="form-name">Registration</p>
+        <input className="form-input" type="text" placeholder="Username" />
+        <input className="form-input" type="text" placeholder="Email Address" />
+        <input className="form-input" type="password" placeholder="Password" />
+        <input className="form-input" type="password" placeholder="Confirm Password" />
+        <button className="submit-button">Register</button>
+        <p className="form-switcher">Already have an account ? <span>Login</span></p>
+      </div>
     </div>
   )
 }
--- frontend/src/components/RestaurantsList.jsx
+++ frontend/src/components/RestaurantsList.jsx
@@ -1,9 +1,10 @@
+import LoadingSpinner from "./LoadingSpinner";
 import Restaurant from "./Restaurant";
 
 export default function RestaurantsList({data}) {
   return (
     <section>
-      {data.length === 0 && <div className="loading-container"><div className="loading-animation"></div></div>}
+      {data.length === 0 && <LoadingSpinner />}
       <div className="restaurants-list">
         {data.length > 0 && data.map((item, i) => {
           return <Restaurant key={i} name={data[i].name} timetable={data[i].timetable} />
