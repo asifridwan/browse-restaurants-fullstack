fad6491ba3762fe77d0dde4ec724623a583039cd
Asif Ridwan <asifridwan@gmail.com>
2022-02-24 16:40:05 +0600

    Added search functionality

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,14 @@
-export default function Navbar() {
+export default function Navbar({searchField, searchTypeAction, searchTypeName}) {
   return (
     <nav>
       <div className="nav-centering">
         <span className="logo-icon"></span>
         <span className="logo-text">Browse Restaurants</span>
       </div>
-      <input className="search-box" type="text" placeholder="Type anything to search...."/>
+      <input className="search-box" type="text" placeholder="Type anything to search...." onChange={searchField}/>
       <div>
-        <label htmlFor="" className="filter-select">Filter By : </label>
-        <select name="" id="" className="filter-type">
-          <option value="">Name</option>
-          <option value="">Date and Time</option>
-        </select>
+        <label className="filter-select">Filter By : </label>
+        <button className="filter-button" onClick={searchTypeAction}>{searchTypeName}</button>
       </div>
       <div className="dropdown">
         <div className="nav-centering">
--- frontend/src/components/RestaurantsList.jsx
+++ frontend/src/components/RestaurantsList.jsx
@@ -1,20 +1,12 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
 import Restaurant from "./Restaurant";
 
-export default function RestaurantsList() {
-  const [restaurants, setRestaurants] = useState([]);
-
-  useEffect(() => {
-    axios.get('http://localhost:4000/restaurants').then(res => setRestaurants(res.data))
-  }, [])
-
+export default function RestaurantsList({data}) {
   return (
     <section>
-      {restaurants.length === 0 && <div className="loading-container"><div className="loading-animation"></div></div>}
+      {data.length === 0 && <div className="loading-container"><div className="loading-animation"></div></div>}
       <div className="restaurants-list">
-        {restaurants.length > 0 && restaurants.map((item, i) => {
-          return <Restaurant key={i} name={restaurants[i].name} timetable={restaurants[i].timetable} />
+        {data.length > 0 && data.map((item, i) => {
+          return <Restaurant key={i} name={data[i].name} timetable={data[i].timetable} />
         })}
       </div>
     </section>
